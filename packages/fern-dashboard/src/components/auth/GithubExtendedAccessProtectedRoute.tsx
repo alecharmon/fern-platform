7dc9b0c85ede08bb5d93c4a8bfc37a5dfdeac70c
sarah bawabe <87032489+sbawabe@users.noreply.github.com>
2025-07-30 11:49:35 -0400

    fix: bypass auth for dashboard (#3312)

--- packages/fern-dashboard/src/components/auth/GithubExtendedAccessProtectedRoute.tsx
+++ packages/fern-dashboard/src/components/auth/GithubExtendedAccessProtectedRoute.tsx
@@ -1,15 +1,11 @@
 import { redirect } from "next/navigation";
 import React from "react";
 
-import checkGitHubPermissions, {
-  checkWritePermissionToRepo,
-} from "@/app/api/github-permissions/handler";
 import { getCurrentSession } from "@/app/services/auth0/getCurrentSession";
 import * as auth0Management from "@/app/services/auth0/management";
 import { Auth0OrgName } from "@/app/services/auth0/types";
 
 import { Page404 } from "../Page404";
-import { AuthorizeGithubModal } from "../docs-page/AuthorizeGithubModal";
 
 export declare namespace GithubExtendedAccessProtectedRoute {
   export interface Props {
@@ -22,8 +18,8 @@ export declare namespace GithubExtendedAccessProtectedRoute {
 
 export const GithubExtendedAccessProtectedRoute = async ({
   orgName,
-  owner,
-  repo,
+  owner: _owner,
+  repo: _repo,
   children,
 }: GithubExtendedAccessProtectedRoute.Props) => {
   const session = await getCurrentSession();
@@ -41,28 +37,28 @@ export const GithubExtendedAccessProtectedRoute = async ({
     return <Page404 />;
   }
 
-  if (owner && repo) {
-    const writePermission = await checkWritePermissionToRepo(
-      session.user.sub,
-      owner,
-      repo
-    );
-    if (!writePermission) {
-      return (
-        <AuthorizeGithubModal
-          open
-          persistent
-          hideTrigger
-          customMessage="You don't have write permission to this repo. Please obtain write permission and re-authorize to continue."
-        />
-      );
-    }
-  }
+  // if (owner && repo) {
+  //   const writePermission = await checkWritePermissionToRepo(
+  //     session.user.sub,
+  //     owner,
+  //     repo
+  //   );
+  //   if (!writePermission) {
+  //     return (
+  //       <AuthorizeGithubModal
+  //         open
+  //         persistent
+  //         hideTrigger
+  //         customMessage="You don't have write permission to this repo. Please obtain write permission and re-authorize to continue."
+  //       />
+  //     );
+  //   }
+  // }
 
-  const { hasRepoAccess } = await checkGitHubPermissions(session.user.sub);
-  if (!hasRepoAccess) {
-    return <AuthorizeGithubModal open persistent hideTrigger />;
-  }
+  // const { hasRepoAccess } = await checkGitHubPermissions(session.user.sub);
+  // if (!hasRepoAccess) {
+  //   return <AuthorizeGithubModal open persistent hideTrigger />;
+  // }
 
   return children;
 };
--- packages/fern-dashboard/src/components/docs-page/GithubProtectedArea.tsx
+++ packages/fern-dashboard/src/components/docs-page/GithubProtectedArea.tsx
@@ -1,17 +1,13 @@
 import { redirect } from "next/navigation";
 
-import checkGitHubPermissions, {
-  checkWritePermissionToRepo,
-} from "@/app/api/github-permissions/handler";
 import { getCurrentSession } from "@/app/services/auth0/getCurrentSession";
 import { GithubSourceRepo } from "@/app/services/github/types";
 
-import { AuthorizeGithubModal } from "./AuthorizeGithubModal";
 import { GithubPermissionsProvider } from "./GithubPermissionsContext";
 
 export async function GithubProtectedArea({
   children,
-  sourceRepo,
+  sourceRepo: _sourceRepo,
 }: {
   children: React.ReactNode;
   sourceRepo?: GithubSourceRepo;
@@ -22,23 +18,29 @@ export async function GithubProtectedArea({
     redirect("/");
   }
 
-  const githubPermissions = await checkGitHubPermissions(session.user.sub);
-  if (!githubPermissions.hasRepoAccess) {
-    return <AuthorizeGithubModal />;
-  }
-
-  const writePermission =
-    sourceRepo?.owner && sourceRepo?.repo
-      ? await checkWritePermissionToRepo(
-          session.user.sub,
-          sourceRepo.owner,
-          sourceRepo.repo
-        )
-      : undefined;
-
   return (
-    <GithubPermissionsProvider writePermission={writePermission}>
+    <GithubPermissionsProvider writePermission={true}>
       {children}
     </GithubPermissionsProvider>
   );
+
+  // const githubPermissions = await checkGitHubPermissions(session.user.sub);
+  // if (!githubPermissions.hasRepoAccess) {
+  //   return <AuthorizeGithubModal />;
+  // }
+
+  // const writePermission =
+  //   sourceRepo?.owner && sourceRepo?.repo
+  //     ? await checkWritePermissionToRepo(
+  //         session.user.sub,
+  //         sourceRepo.owner,
+  //         sourceRepo.repo
+  //       )
+  //     : undefined;
+
+  // return (
+  //   <GithubPermissionsProvider writePermission={writePermission}>
+  //     {children}
+  //   </GithubPermissionsProvider>
+  // );
 }
