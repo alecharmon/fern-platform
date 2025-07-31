58f78803fd31da4b67b297b9ab25716dc1f7352a
eden <64514273+eyw520@users.noreply.github.com>
2025-07-31 15:22:29 +0200

    feat(askfern): Add auth to `/chat` endpoint for authed domains. (#3317)

--- packages/fern-docs/bundle/src/app/[host]/[domain]/api/fern-docs/search/v2/chat/route.ts
+++ packages/fern-docs/bundle/src/app/[host]/[domain]/api/fern-docs/search/v2/chat/route.ts
@@ -4,6 +4,7 @@ import { createOpenAI } from "@ai-sdk/openai";
 import { UIMessage } from "ai";
 
 import { createCachedDocsLoader } from "@fern-api/docs-loader";
+import { createGetAuthStateEdge } from "@fern-api/docs-server";
 import {
   getFaiOrigin,
   openaiApiKey,
@@ -37,6 +38,16 @@ export async function POST(req: NextRequest) {
   const createdAt = new Date();
   const host = req.nextUrl.host;
   const domain = getDocsDomainEdge(req);
+
+  const { getAuthState } = await createGetAuthStateEdge(req);
+
+  const pathname = req.nextUrl.pathname;
+  const authState = await getAuthState(pathname);
+
+  if (!authState.authed) {
+    return NextResponse.json("Unauthorized", { status: 401 });
+  }
+
   const loader = await createCachedDocsLoader(host, domain);
   const metadata = await loader.getMetadata();
   if (metadata == null) {
