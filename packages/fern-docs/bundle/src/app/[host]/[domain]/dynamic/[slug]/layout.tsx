abeeccbc001874a81c7fd897177b0929e94a710d
Catherine Deskur <46695336+chdeskur@users.noreply.github.com>
2025-08-17 10:22:15 -0400

    chore(docs): use full metadata on dynamic layout (#3418)

--- packages/fern-docs/bundle/src/app/[host]/[domain]/dynamic/[slug]/layout.tsx
+++ packages/fern-docs/bundle/src/app/[host]/[domain]/dynamic/[slug]/layout.tsx
@@ -1,9 +1,12 @@
 import "server-only";
 
+import { Metadata } from "next/types";
+
 import { createCachedDocsLoader } from "@fern-api/docs-loader";
 import { slugjoin } from "@fern-api/fdr-sdk/navigation";
 
 import { getFernToken } from "@/app/fern-token";
+import { generateMetadataFromPage } from "@/components/seo";
 import SharedPage from "@/components/shared-page";
 
 export default async function DynamicPage(props: {
@@ -17,4 +20,12 @@ export default async function DynamicPage(props: {
     await getFernToken()
   );
   return <SharedPage loader={loader} slug={slugjoin(slug)} />;
+}
+
+export async function generateMetadata(props: {
+  params: Promise<{ host: string; domain: string; slug: string }>;
+}): Promise<Metadata> {
+  const { host, domain, slug } = await props.params;
+  const loader = await createCachedDocsLoader(host, domain);
+  return generateMetadataFromPage({ loader, slug: slugjoin(slug) });
 }
