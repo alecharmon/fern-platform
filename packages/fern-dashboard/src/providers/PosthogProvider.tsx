4656c684c088347bda9067df89af5f793aa3a3ad
Stephen Chen <hello@stephenc.me>
2025-09-16 10:29:07 -0400

    fix(dashboard): orgName should not be set before PostHog init (#3661)

--- packages/fern-dashboard/src/providers/PosthogProvider.tsx
+++ packages/fern-dashboard/src/providers/PosthogProvider.tsx
@@ -1,14 +1,15 @@
 "use client";
 
+import { useParams } from "next/navigation";
 import React, { useEffect } from "react";
 
 import posthog from "posthog-js";
 import { PostHogProvider as PHProvider } from "posthog-js/react";
 
 import { Auth0SessionData } from "@/app/services/auth0/getCurrentSession";
+import { Auth0OrgName } from "@/app/services/auth0/types";
 import { PostHogIdentify } from "@/components/posthog/PostHogIdentify";
 import { PostHogPageView } from "@/components/posthog/PostHogPageView";
-import { PostHogSuperProperties } from "@/components/posthog/PostHogSuperProperties";
 import { isProduction } from "@/utils/environment";
 
 export declare namespace PostHogProvider {
@@ -21,6 +22,9 @@ export declare namespace PostHogProvider {
 export function PostHogProvider({ session, children }: PostHogProvider.Props) {
   const isPosthogTrackingEnabled =
     process.env.NEXT_PUBLIC_POSTHOG_TRACKING_ENABLED === "true";
+
+  const params = useParams();
+  const orgName = params.orgName as Auth0OrgName;
 
   useEffect(() => {
     if (process.env.NEXT_PUBLIC_POSTHOG_KEY == null) {
@@ -43,13 +47,19 @@ export function PostHogProvider({ session, children }: PostHogProvider.Props) {
     });
   }, [isPosthogTrackingEnabled, session?.user.email]);
 
+  useEffect(() => {
+    if (isPosthogTrackingEnabled && orgName) {
+      // Register organization as a super property and include it in ALL events automatically
+      posthog.register({ orgName: orgName });
+    }
+  }, [isPosthogTrackingEnabled, orgName]);
+
   return (
     <PHProvider client={posthog}>
       {isPosthogTrackingEnabled && (
         <>
           <PostHogPageView />
           <PostHogIdentify user={session?.user} />
-          <PostHogSuperProperties />
         </>
       )}
       {children}
