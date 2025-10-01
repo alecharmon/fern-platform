118acdef2af575000a5ffc21e37154da0f04a21d
Deep Singhvi <deep@buildwithfern.com>
2025-10-01 21:30:30 +0100

    fix fdr lambda deploy (#3884)

--- servers/fdr-lambda-deploy/bin/fdr-lambda-deploy.ts
+++ servers/fdr-lambda-deploy/bin/fdr-lambda-deploy.ts
@@ -61,7 +61,7 @@ async function main() {
           version,
           environmentType,
           environmentInfo,
-          "sg-c3cb3dd2",
+          "sg-0158802587ada8261",
           {
             env: { account: "985111089818", region: "us-east-1" },
           }
@@ -74,7 +74,7 @@ async function main() {
           version,
           environmentType,
           environmentInfo,
-          "sg-0a1b2c3d4e5f6g7h8", // RDS Proxy security group ID for prod
+          "sg-c3cb3dd2",
           {
             env: { account: "985111089818", region: "us-east-1" },
           }
