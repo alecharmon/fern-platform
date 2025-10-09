e27bba0716ba810782d468b79ce5507527bc2c5c
David Konigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2025-10-09 09:31:16 -0400

    feat(ci): Add e2e container tests for FDR (#3958)
    Co-authored-by: Thomas Baker <thomas@buildwithfern.com>

--- servers/fdr/src/__test__/ete/ete.test.ts
+++ servers/fdr/src/__test__/ete/ete.test.ts
@@ -1,26 +1,24 @@
-/* eslint-disable vitest/expect-expect */
+import { it, describe } from "vitest";
 
 const PORT = 8080;
 
-it("check health", async () => {
-    // register empty definition
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-    for (let i = 0; i < 10; ++i) {
-        await sleep(20_000);
-        try {
-            const response = await fetch(`http://localhost:${PORT}/health`);
-            if (response.status === 200) {
-                return;
-            } else {
-                console.log(`Received status ${response.status}`);
-            }
-        } catch (err) {
-            console.log(err);
+describe("FDR container tests", () => {
+    it("check health", async () => {
+        const response = await fetch(`http://0.0.0.0:${PORT}/health`);
+        if (!response.ok) {
+            throw new Error(`Health check failed with status ${response.status}`);
         }
-    }
-    throw new Error("Failed to make successfull request");
-}, 100_000);
+    });
 
-function sleep(ms: number): Promise<void> {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
+    it("test ETE connection to DB", async () => {
+        const response = await fetch(`http://localhost:${PORT}/registry/api/latest/load/1`);
+        const responseText = await response.text();
+        console.log(`Response body: '${responseText}'`);
+
+        if (responseText !== "Not Found") {
+            throw new Error(`Expected 'Not Found' but got '${responseText}'`);
+        }
+    });
+});
--- servers/fdr/src/__test__/ete/vitest.config.ts
+++ servers/fdr/src/__test__/ete/vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
-        globals: true,
-        globalSetup: ["src/__test__/ete/setupDockerizedFdr.ts"]
+        globals: true
     }
 });
