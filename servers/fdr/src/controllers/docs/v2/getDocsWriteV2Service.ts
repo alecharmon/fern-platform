309880147f08429198596ebe8adc99ab9a58c366
Kenny Derek <pickypulsars@gmail.com>
2025-11-04 13:49:30 -0500

    fix(fdr): skip dns check (#4751)

--- servers/fdr/src/controllers/docs/v2/getDocsWriteV2Service.ts
+++ servers/fdr/src/controllers/docs/v2/getDocsWriteV2Service.ts
@@ -309,19 +309,7 @@ export function getDocsWriteV2Service(app: FdrApplication): DocsV2WriteService {
          * IMPORTANT NOTE:
          * vercel cache is not shared between custom domains, so we need to revalidate on EACH custom domain individually
          */
-        const liveCustomUrls = [];
-        for (const customUrl of docsRegistrationInfo.customUrls) {
-          const isLive = await checkDNSConfigured(customUrl);
-          if (isLive || app.config.localModeOverride) {
-            liveCustomUrls.push(customUrl);
-          } else {
-            app.logger.info(
-              `Skipping revalidation for ${customUrl.getFullUrl()} - domain is not live`
-            );
-          }
-        }
-
-        const urls = [docsRegistrationInfo.fernUrl, ...liveCustomUrls];
+        const urls = [docsRegistrationInfo.fernUrl, ...docsRegistrationInfo.customUrls];
 
         for (const url of urls) {
           try {
@@ -512,36 +500,4 @@ export function getDocsWriteV2Service(app: FdrApplication): DocsV2WriteService {
       return res.send();
     },
   });
-}
-
-async function checkDNSConfigured(baseUrl: ParsedBaseUrl) {
-  try {
-    const response = await fetch(
-      `https://${baseUrl.hostname}${baseUrl.path || ""}`,
-      {
-        method: "HEAD",
-        redirect: "manual",
-      }
-    );
-
-    return true;
-  } catch (error) {
-    if (error instanceof Error) {
-      if (
-        error.message.includes("getaddrinfo ENOTFOUND") ||
-        (error.name === "TypeError" && error.message.includes("fetch"))
-      ) {
-        return false;
-      }
-    } else if (
-      typeof error === "object" &&
-      error !== null &&
-      "code" in error &&
-      error.code === "ENOTFOUND"
-    ) {
-      return false;
-    }
-
-    return true;
-  }
 }
