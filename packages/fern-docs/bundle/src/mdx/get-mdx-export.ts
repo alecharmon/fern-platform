a64b457f9eae2a59aba8ed7ddf2f585e3bc6633b
Catherine Deskur <46695336+chdeskur@users.noreply.github.com>
2025-11-07 16:44:20 -0500

    chore: check for plaintext (#4887)

--- packages/fern-docs/bundle/src/mdx/get-mdx-export.ts
+++ packages/fern-docs/bundle/src/mdx/get-mdx-export.ts
@@ -2,7 +2,7 @@ import { isToc, type TableOfContentsItem } from "@fern-docs/mdx";
 import { getMDXExport as getMDXExportOriginal, type MDXContentProps } from "mdx-bundler/client";
 
 export function getMDXExport(
-    mdx: { code: string } | undefined,
+    mdx: { code: string; engine?: "esbuild" | "next-remote" | "plaintext" } | undefined,
     useMDXComponents: () => any = () => ({})
 ):
     | {
@@ -13,12 +13,19 @@ export function getMDXExport(
     if (mdx == null) {
         return undefined;
     }
-    return typeof mdx !== "string"
-        ? getMDXExportOriginal(mdx.code, {
-              // allows us to use MDXProvider to pass components to children
-              MdxJsReact: { useMDXComponents }
-          })
-        : undefined;
+
+    if (typeof mdx === "string") {
+        return undefined;
+    }
+
+    if (mdx.engine === "plaintext") {
+        return undefined;
+    }
+
+    return getMDXExportOriginal(mdx.code, {
+        // allows us to use MDXProvider to pass components to children
+        MdxJsReact: { useMDXComponents }
+    });
 }
 
 export function asToc(unknown: unknown): TableOfContentsItem[] {
