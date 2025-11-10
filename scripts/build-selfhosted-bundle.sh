0b43e1b51e2a4d1d0351507133fc6100e15be79b
Colton Berry <10538007+coltondotio@users.noreply.github.com>
2025-11-10 17:43:56 -0500

    fix(docs): fix self hosted docker image (#4319)

--- scripts/build-selfhosted-bundle.sh
+++ scripts/build-selfhosted-bundle.sh
@@ -16,7 +16,7 @@ if [ -f "$ENV_LOCAL_PATH" ]; then
 fi
 
 # run the build process
-NODE_ENV=production pnpm --filter=@fern-docs/bundle docs:build:selfhosted
+NODE_OPTIONS="--max-old-space-size=8192" NODE_ENV=production pnpm --filter=@fern-docs/bundle docs:build:selfhosted
 cp -r packages/fern-docs/bundle/.next/static packages/fern-docs/bundle/.next/standalone/packages/fern-docs/bundle/.next
 find packages/fern-docs/bundle/.next -depth -mindepth 1 -not -path "packages/fern-docs/bundle/.next/standalone*" -exec rm -rf {} \;
 rm -rf packages/fern-docs/bundle/.next/standalone/node_modules/.pnpm/esbuild@0.25.9 && rm -rf packages/fern-docs/bundle/.next/standalone/node_modules/.pnpm/@esbuild+linux-x64@0.25.9
