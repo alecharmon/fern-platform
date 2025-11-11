eae02a8808d6b744e2ad70d785432d95c9d3ab5f
sarah bawabe <87032489+sbawabe@users.noreply.github.com>
2025-11-11 16:29:36 -0500

    ui(dashboard): tweak editor section header (#4852)
    Fixes FER-
    
    ## Short description of the changes made
    - quick win to tweak header of editor section so that warning notes look less garbo
    
    ## What was the motivation & context behind this PR?
    - ui fix!
    
    ## How has this PR been tested?
    - tested on happy paths, empty paths, and warning paths on desktop and mobile

--- packages/fern-dashboard/src/components/docs-page/visual-editor-section/VisualEditorHeader.tsx
+++ packages/fern-dashboard/src/components/docs-page/visual-editor-section/VisualEditorHeader.tsx
@@ -2,15 +2,15 @@ import { BetaBadge } from "../BetaBadge";
 
 export function VisualEditorHeader({ rightContent }: { rightContent?: React.ReactNode }) {
     return (
-        <div className="flex items-center justify-center md:justify-between gap-4 flex-col md:flex-row">
-            <div className="flex flex-col text-center md:text-left">
-                <div className="flex items-center justify-center md:justify-start gap-2 text-lg font-semibold">
+        <div className="flex items-center lg:items-start justify-center lg:justify-between gap-4 flex-col lg:flex-row">
+            <div className="flex flex-col text-center lg:text-left">
+                <div className="flex items-center justify-center lg:justify-start gap-2 text-lg font-semibold">
                     Fern Editor
                     <BetaBadge />
                 </div>
                 <p className="text-muted-foreground text-sm">Modify your documentation without touching code.</p>
             </div>
-            {rightContent}
+            <div className="lg:max-w-1/2 max-w-[400px] w-fit">{rightContent}</div>
         </div>
     );
 }
