f8f0919e5863b1aef47d02e35779e5e38f7bc98c
Alec <alecharmon@outlook.com>
2025-12-09 11:10:38 -0500

    Actively filter middleware and _next/ traces for sentry (#5766)
    Co-authored-by: alecharmon <alec@buildwithfern.com>

--- packages/fern-dashboard/sentry.base.config.ts
+++ packages/fern-dashboard/sentry.base.config.ts
@@ -10,6 +10,22 @@ export const baseConfig: SentryBaseConfig = {
     environment: process.env.NODE_ENV,
     sendDefaultPii: true,
     tracesSampleRate: 1,
+    tracesSampler: (samplingContext) => {
+        const name = samplingContext.transactionContext?.name?.toLowerCase() ?? "";
+
+        // Heavily sample middleware routes
+        if (name.includes("middleware")) {
+            return 0.01; // sample 1% of middleware traces
+        }
+
+        // drastically drop _next/static or other noise
+        if (name.includes("_next/")) {
+            return 0.001;
+        }
+
+        // Use env or default sampling rate for everything else
+        return parseFloat(process.env.DASHBOARD_SENTRY_TRACES_SAMPLE_RATE ?? "0.1");
+    },
     enableLogs: true,
     serverName: process.env.HOSTNAME || "unknown",
     integrations: [Sentry.consoleLoggingIntegration({ levels: ["debug", "log", "warn", "error"] })]
