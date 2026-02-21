ce0fbfe9efce568f4576d4d300fa35eb7fcb64bb
Deep Singhvi <deep@buildwithfern.com>
2026-02-21 02:02:00 +0000

    refactor(fdr): switch runtime from Node.js to Bun (#7355)

--- servers/fdr/scripts/run.sh
+++ servers/fdr/scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 prisma migrate deploy --schema /app/servers/fdr/prisma/schema.prisma
-node --loader /app/servers/fdr/ts-loader.js --experimental-specifier-resolution=node /app/servers/fdr/dist/server.js
+bun run /app/servers/fdr/dist/server.js
