d321ca1c8329c82e4a9332363d39524221313b4a
Deep Singhvi <deep@buildwithfern.com>
2026-03-11 16:12:57 +0000

    Revert "fix(fdr): set numCacheReplicasPerShard to 1 for cluster mode compatibility" (#8363)

--- servers/fdr-deploy/scripts/fdr-deploy-stack.ts
+++ servers/fdr-deploy/scripts/fdr-deploy-stack.ts
@@ -228,7 +228,7 @@ export class FdrDeployStack extends Stack {
             cacheName: options.cacheName,
             IVpc: vpc,
             numCacheShards: 1,
-            numCacheReplicasPerShard: 1,
+            numCacheReplicasPerShard: 0,
             clusterMode: "enabled",
             cacheNodeType: options.cacheNodeType,
             envType: environmentType,
